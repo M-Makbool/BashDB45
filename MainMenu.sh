61a717d0bf1fd6db0ee0abf00f2a20f928b2107e
Eslam Ahmed <eslam.ahmed930950@gmail.com>
2024-11-27 15:23:31 +0200

    I made CLI appears in one screen and added function name_chekcer

--- MainMenu.sh
+++ MainMenu.sh
@@ -13,6 +13,7 @@
 
 main_menu() { 
     while true ; do
+        clear;
         echo "---Main Menu---"
         echo "1- Create Database"
         echo "2- List Databases"
@@ -20,30 +21,43 @@ main_menu() {
         echo "4- Drop Database"
         echo "5- Exit"
         echo "--------------"
-
+        echo $MESSEGE
         read -p "Choose What you want to do: " option
         option2=$(echo "$option" | tr '[:upper:]'  '[:lower:]')
-
+        MESSEGE=''
         case "$option2" in 
-            "1"|"create"|"create database") create_database;;
-            "2"|"list"|"list database") list_database;;
-            "3"|"connect"|"connect database") connect_database;;
-            "4"|"drop"|"drop database") drop_database;;
+            "1"|"create"|"create database") MESSEGE=$(create_database);;
+            "2"|"list"|"list databases") MESSEGE=$(list_database);;
+            "3"|"connect"|"connect database") MESSEGE=$(connect_database);;
+            "4"|"drop"|"drop database") MESSEGE=$(drop_database);;
             "5"|exit) break;;
-            *) echo "Wrong choice, Please write a number from 1 to 5 or the option you want to do, ex: 1 or create database." ;;
+            *) MESSEGE=$( "Wrong choice, Please write a number from 1 to 5 or the option you want to do, ex: 1 or create database.") ;;
 esac
 done
 }
 
+name_checker(){
+
+    if [[ "$1" =~ ^[0-9] || "$1" =~ [^a-zA-Z0-9] ]]; then
+        echo Please enter a valid name with no special chars and does not start with a number
+        return 1
+    else
+        return 0
+    fi
+}
 create_database () {
     read -p "Enter Database Name: " db_name
-    #name checker
-    if [ -z "$db_name" ]; then 
-        echo "Database Name can NOT be empty"    
-    elif [ -d "$BASE_DIR/$db_name" ]; then    
-        echo "Database already exists"
-    else 
-        mkdir -p "$BASE_DIR/$db_name" && echo "Database '$db_name' created succesfully"    
+    MESSEGE=$(name_checker $db_name)
+    if [ $? -eq 0 ]; then
+        if [ -z "$db_name" ]; then 
+            echo "Database Name can NOT be empty"    
+        elif [ -d "$BASE_DIR/$db_name" ]; then    
+            echo "Database already exists"
+        else 
+            mkdir -p "$BASE_DIR/$db_name" && echo "Database '$db_name' created succesfully"    
+        fi
+    else
+    echo $MESSEGE;
     fi
 }
 
@@ -55,24 +69,33 @@ list_database(){
 
 connect_database(){
     read -p "Please enter a database name to connect: " db_name
-    #name checker
-    if [ -d "$BASE_DIR/$db_name" ]; then 
-        echo "Database $database is connected" 
-        db_menu "$BASE_DIR/$db_name"
-    else 
-        echo "Database does NOT exist"  
-    fi  
+    MESSEGE=$(name_checker $db_name)
+    if [ $? -eq 0 ]; then
+        if [ -d "$BASE_DIR/$db_name" ]; then 
+            echo "Database $database is connected" 
+            db_menu "$BASE_DIR/$db_name"
+        else 
+            echo "Database does NOT exist"  
+        fi  
+    else
+    echo $MESSEGE;
+    fi    
 }
 
 
 drop_database(){
     read -p "Enter the database name you want to delete: " db_name
-    #name checker
-    if [ -d "$BASE_DIR/$db_name" ]; then
-        rm -r "$BASE_DIR/$db_name" && echo "Database succesfully dropped"
-    else 
-    echo "database does not exist!"    
+    MESSEGE=$(name_checker $db_name)
+    if [ $? -eq 0 ]; then
+        if [ -d "$BASE_DIR/$db_name" ]; then
+            rm -r "$BASE_DIR/$db_name" && echo "Database successfully dropped"
+        else 
+        echo "database does not exist!"    
+        fi
+    else
+    echo $MESSEGE;
     fi
 }
+
 
 main_menu
