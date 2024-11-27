cf3d6645a3aabcf0c08e2a7fefd85fd7b5ce9911
Mohammed Makbool <m.makbool@outlook.sa>
2024-11-27 15:28:17 +0200

    Structured the database_menu funcations

---
+++ DatabaseMenu.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+<< INFO
+Mohammed Makbool
+23-11-2024
+iTi Bash Project
+INFO
+
+function create_table(){
+    echo table created;
+}
+function list_table(){
+    echo table list;
+}
+function drop_table(){
+    echo table drop;
+}
+function insert_table(){
+    echo table insert;
+}
+function select_table(){
+    echo table select;
+}
+function delete_table(){
+    echo table delete;
+}
+function update_table(){
+    echo table update;
+}
+
+while true
+do
+    clear;
+    echo '1- Create Table'
+    echo '2- List Tables'
+    echo '3- Drop Table'
+    echo '4- Insert into Table'
+    echo '5- Select From Table'
+    echo '6- Delete From Table'
+    echo '7- Update Table'
+    echo '8- Main Menu'
+    echo '9- Exit'
+    echo "--------------"
+    echo $Mess
+    read -p "Choose What you want to do: " option
+    option2=$(echo "$option" | tr '[:upper:]'  '[:lower:]')
+    Mess=''
+    case "$option2" in 
+        "1"|"create"|"create table") Mess=`create_table`;;
+        "2"|"list"|"list tables") Mess=`list_table`;;
+        "3"|"drop"|"drop table") Mess=`drop_table`;;
+        "4"|"insrt"|"insert into table") Mess=`insert_table`;;
+        "5"|"select"|"select from table") Mess=`select_table`;;
+        "6"|"delete"|"delete from table") Mess=`delete_table`;;
+        "7"|"update"|"update table") Mess=`update_table`;;
+        "8"|"main"|"main menu") ./MainMenu.sh;;
+        "9"|exit) break;;
+        *) Mess="Wrong choice, Please write a number from 1 to 7 or the option you want to do, ex: 1 or create table." ;;
+    esac
+done;
